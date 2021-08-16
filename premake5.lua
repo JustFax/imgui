29b2a3ec78fc623b49e74236477d04b1bab88db3
Fax <eemeli.niemenlehto@gmail.com>
2021-08-16 20:15:53 +0300

    Added premake file

--- premake5.lua
+++ premake5.lua
@@ -22,13 +22,11 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
